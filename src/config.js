00ee05b1c21107833aa2db151fc17751d1ababa7
Adriana <adriana.luz.guerra@gmail.com>
2020-06-12 11:00:55 -0500

    Setting up our React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "thebkrasri-notes"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://1gxuiapeq7.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_pELW6fZ6Q",
+      APP_CLIENT_ID: "7ijuijl7899fqnmtrb1abc4o9n",
+      IDENTITY_POOL_ID: "us-east-2:50f00b92-6c75-4329-8b27-0b5fdc6a6b75"
+    }
+  };
