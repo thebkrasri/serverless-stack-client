1fe13daf2f79440ecb1bcb1bb2324e1a2aeacd03
Adriana <adriana.luz.guerra@gmail.com>
2020-06-14 11:45:07 -0500

    Building our React app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Auth } from "aws-amplify";
+import { AppContext } from "./libs/contextLib";
+import { onError } from "./libs/errorLib";
 import { LinkContainer } from "react-router-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import Routes from "./Routes";
 import "./App.css";
 
 function App() {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const history = useHistory();
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    }
+    catch (e) {
+      if (e !== 'No current user') {
+        onError(e);
+      }
+    }
+
+    setIsAuthenticating(false);
+  }
+
+  async function handleLogout() {
+    await Auth.signOut();
+
+    userHasAuthenticated(false);
+    history.push("/login");
+  }
+
   return (
+    !isAuthenticating &&
     <div className="App container">
       <Navbar fluid collapseOnSelect>
         <Navbar.Header>
@@ -17,16 +50,30 @@ function App() {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <LinkContainer to="/signup">
-              <NavItem>Signup</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <NavItem>Login</NavItem>
-            </LinkContainer>
+            {isAuthenticated
+              ? (
+                <>
+                  <LinkContainer to="/settings">
+                    <NavItem>Settings</NavItem>
+                  </LinkContainer>
+                  <NavItem onClick={handleLogout}>Logout</NavItem>
+                </>
+              )
+              : <>
+                <LinkContainer to="/signup">
+                  <NavItem>Signup</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                  <NavItem>Login</NavItem>
+                </LinkContainer>
+              </>
+            }
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Routes />
+      <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+        <Routes />
+      </AppContext.Provider>
     </div>
   );
 }
--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
 export default {
-    s3: {
-      REGION: "us-east-2",
-      BUCKET: "thebkrasri-notes"
-    },
-    apiGateway: {
-      REGION: "us-east-2",
-      URL: "https://1gxuiapeq7.execute-api.us-east-2.amazonaws.com/prod"
-    },
-    cognito: {
-      REGION: "us-east-2",
-      USER_POOL_ID: "us-east-2_pELW6fZ6Q",
-      APP_CLIENT_ID: "7ijuijl7899fqnmtrb1abc4o9n",
-      IDENTITY_POOL_ID: "us-east-2:50f00b92-6c75-4329-8b27-0b5fdc6a6b75"
-    }
-  };
+  STRIPE_KEY: "pk_test_51GsxMOKrMoWcn0H7rljnblTSIXrfeNyFnUDfjCY9Mxred4adFzOLMMiS0W2YxVlPTewMLJHEiYHZa92HIjJr90i0002KakpoKz",
+  MAX_ATTACHMENT_SIZE: 5000000,
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "thebkrasri-notes"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://1gxuiapeq7.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_pELW6fZ6Q",
+    APP_CLIENT_ID: "7ijuijl7899fqnmtrb1abc4o9n",
+    IDENTITY_POOL_ID: "us-east-2:50f00b92-6c75-4329-8b27-0b5fdc6a6b75"
+  }
+};
